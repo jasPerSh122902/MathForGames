0310dcbbb70c083c1550ab6e1b2223905711c21b
jasper <s218005@students.aie.edu>
2021-11-09 09:21:16 -0600

    addded files to master

---
+++ AABBCollider.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibaray;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class AABBCollider : Collider
+    {
+        private float _width;
+        public float _height;
+
+        /// <summary>
+        /// The size of ths collider on the x axis
+        /// </summary>
+        public float Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        /// <summary>
+        /// The size of this collider on the y axis
+        /// </summary>
+        public float Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        /// <summary>
+        /// The farthest left x position of the collider
+        /// </summary>
+        public float Left
+        {
+            get
+            {
+                return Owner.LocalPosistion.X + -(_width / 2);
+            }
+        }
+
+        /// <summary>
+        /// The farthest right x posistion of the collider
+        /// </summary>
+        public float Right
+        {
+            get
+            {
+                return Owner.LocalPosistion.X + _width / 2;
+            }
+        }
+
+        /// <summary>
+        /// The fartheset top y position of the collider
+        /// </summary>
+        public float Top
+        {
+            get
+            {
+                return Owner.LocalPosistion.Y + -(_height / 2);
+            }
+        }
+
+        /// <summary>
+        /// The farthesest bottom y posistion of the collider
+        /// </summary>
+        public float Bottom
+        {
+            get
+            {
+                return Owner.LocalPosistion.Y + Height / 2;
+            }
+        }
+
+        /// <summary>
+        /// The constructor for AABB collider
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="owner"></param>
+        public AABBCollider(float width, float height, Actor owner) : base(owner, ColliderType.AABB)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public override bool CheckCollisionAABB(AABBCollider other)
+        {
+            //Return false if this owner is checking for a collision against itself
+            if (other.Owner == Owner)
+                return false;
+
+            //This checkes each oppossit side is lest than a nother (normaly the first two are there for the Other or second object is less than the main object...
+            //... The last two is to check if the main object is lest than the second object.
+            //Return true if There is an overlap between boxes.
+            if (other.Left <= Right &&
+                other.Top <= Bottom &&
+                Left <= other.Right &&
+                Top <= other.Bottom)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Is the draw function
+        /// </summary>
+        public override void Draw()
+        {
+            //is meant to draw the collider that is the rectangle
+            Raylib.DrawRectangleLines((int)Left, (int)Top, (int)Width, (int)Height, Color.PINK);
+        }
+
+        public override bool CheckCollisionCircle(CircleCollider other)
+        {
+            return other.CheckCollisionAABB(this);
+        }
+    }
+}
+
---
+++ Enemey.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibaray;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class Enemey : Actor
+    {
+        private float _speed;
+        private Vector3 _velocity;
+        private Player _player;
+        private int _health = 1;
+        public Scene _scene;
+
+
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public Vector3 Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public int Health
+        {
+            get { return _health; }
+            set { _health = value; }
+        }
+
+
+        public Enemey(float x, float y, float z, float speed, int health, Player player, string name = "Enemy", Shape shape = Shape.CUBE)
+            : base(x, y, z, speed, name, shape)
+        {
+            //i need to the player = palyer I need to get the this.
+            _speed = speed;
+            _player = player;
+            _health = health;
+            health = 1;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            //Create a vector tht stores the move input
+            Vector3 moveDirection = new Vector3();
+
+            moveDirection = _player.LocalPosistion - LocalPosistion;
+
+            //caculates the veclocity 
+            Velocity = moveDirection.Normalized * Speed * deltaTime;
+
+            if (GetTargetInSight())
+                LocalPosistion += Velocity;
+
+            base.Update(deltaTime);
+
+
+        }
+
+        /// <summary>
+        /// Get the Sight of the enemy 
+        /// </summary>
+        /// <returns>return the feild of vey for the enemy and returns
+        /// the possiple distace to get of of veiw</reddddturns>
+        public bool GetTargetInSight()
+        {
+
+            float distace;
+
+            //makes dirctionofTarget the Positin of palyer and Position of enemy and normalized
+            //...The result.
+            Vector3 directionOfTarget = (_player.LocalPosistion - LocalPosistion).Normalized;
+
+            distace = (_player.LocalPosistion - LocalPosistion).Magnitude;
+            //55 is the degress increase it for the feild of view
+            return (Math.Acos(Vector3.DotProduct(directionOfTarget, Forward))
+                * 180 / Math.PI < 55) && distace < 150; 
+        }
+
+        public void Oncollision(Actor actor)
+        {
+            if (actor is Bullet)
+            {
+                _health -= 1;
+                Console.WriteLine("o2iakjdflaskjdflaskdjflaskjdf");
+                if (_health == 0)
+                {
+
+                }
+            }
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+            // Collider.Draw();
+        }
+    }
+}
+
