1d6b91354644703cb255cbc7261a46c799ae5766
jasper <s218005@students.aie.edu>
2021-11-09 10:03:48 -0600

    This is the fills added to my computor so i can work on them.

---
+++ MathLibaray/Matrix4.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibaray
+{
+    public struct Matrix4
+    {
+        public float M00, M01, M02, M03, M10, M11, M12, M13, M20, M21, M22, M23, M30, M31, M32 , M33;
+
+        //The basic constrctor of the Matrix3
+        public Matrix4(float m00, float m01, float m02, float m03,
+                       float m10, float m11, float m12, float m13,
+                       float m20, float m21, float m22, float m23,
+                       float m30, float m31, float m32, float m33)
+        {
+            //setting each of the M's to there respected M
+            M00 = m00; M01 = m01; M02 = m02; M03 = m03;
+            M10 = m10; M11 = m11; M12 = m12; M13 = m13;
+            M20 = m20; M21 = m21; M22 = m22; M23 = m23;
+            M30 = m30; M31 = m31; M32 = m32; M33 = m33;
+        }
+
+        //Made the Matrix3 equal the Identity value do i dont have to call constucor over again.
+        public static Matrix4 Identity
+        {
+            get
+            {
+                //is all of the places in the matrix;
+                return new Matrix4(1, 0, 0, 0,
+                                   0, 1, 0, 0,
+                                   0, 0, 1, 0,
+                                   0, 0, 0, 1);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new matrix that has been rotated by the given value in radians
+        /// </summary>
+        /// <param name="radians">The result of the rotation</param>
+        public static Matrix4 CreateRotationZ(float radians)
+        {
+            return new Matrix4((float)Math.Cos(radians), (float)Math.Sin(radians), 0, 0,
+                               (float)Math.Sin(radians), (float)Math.Cos(radians), 0, 0,
+                                -(float)Math.Cos(radians), (float)Math.Sin(radians), 1, 0,
+                                0, 0, 0, 1);
+        }
+        /// <summary>
+        /// Creates a new matrix that has been rotated by the given value in radians
+        /// </summary>
+        /// <param name="radians">The result of the rotation</param>
+        public static Matrix4 CreateRotationY(float radians)
+        {
+            return new Matrix4((float)Math.Cos(radians), -(float)Math.Sin(radians), 0, 0,
+                               (float)Math.Sin(radians), (float)Math.Cos(radians), 0, 0,
+                                (float)Math.Cos(radians), (float)Math.Sin(radians), 1, 0,
+                                0, 0, 0, 1);
+        }
+
+        /// <summary>
+        /// Creates a new matrix that has been rotated by the given value in radians
+        /// </summary>
+        /// <param name="radians">The result of the rotation</param>
+        public static Matrix4 CreateRotationX(float radians)
+        {
+            return new Matrix4((float)Math.Cos(radians), (float)Math.Sin(radians), 0, 0,
+                               -(float)Math.Sin(radians), (float)Math.Cos(radians), 0, 0,
+                                (float)Math.Cos(radians), (float)Math.Sin(radians), 1, 0,
+                                0, 0, 0, 1);
+        }
+
+        /// <summary>
+        /// Creates a new matrix that has been translated by the given value
+        /// </summary>
+        /// <param name="x">The x position of the new matrix</param>
+        /// <param name="y">The y position of the new matrix</param>
+        public static Matrix4 CreateTranslation(float x, float y, float z)
+        {
+            return new Matrix4(1, 0, 0, x,
+                               0, 1, 0, y,
+                               0, 0, 1, z,
+                               0, 0, 0, 1);
+        }
+        /// <summary>
+        /// Creates a new matrix that has been translated by the given value
+        /// </summary>
+        /// <param name="x">The x position of the new matrix</param>
+        /// <param name="y">The y position of the new matrix</param>
+        public static Matrix4 CreateTranslation(float x, float y)
+        {
+            return new Matrix4(1, 0, 0, x,
+                               0, 1, 0, y,
+                               0, 0, 1, 0,
+                               0, 0, 0, 1);
+        }
+
+        /// <summary>
+        /// Creates a new matrix that has been scaled by the given value
+        /// </summary>
+        /// <param name="x">The value to use to scale the matrix in the x axis</param>
+        /// <param name="y">The value to use to scale the matrix in the y axis</param>
+        /// <returns>The result of the scale</returns>
+        public static Matrix4 CreateScale(float x, float y, float z)
+        {
+            return new Matrix4(x, 0, 0, 0,
+                               0, y, 0, 0,
+                               0, 0, z, 0,
+                               0, 0, 0, 1);
+        }
+        /// <summary>
+        /// Adds the Matrix3
+        /// </summary>
+        /// <param name="lhs">left hand Matrix</param>
+        /// <param name="rhs">Right hand Matrix</param>
+        /// <returns>the added Matriexes</returns>
+        public static Matrix4 operator +(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4(lhs.M00 + rhs.M00, lhs.M01 + rhs.M01, lhs.M02 + rhs.M02, lhs.M03 + rhs.M03,
+                               lhs.M10 + rhs.M10, lhs.M11 + rhs.M11, lhs.M12 + rhs.M12, lhs.M13 + rhs.M13,
+                               lhs.M20 + rhs.M20, lhs.M21 + rhs.M21, lhs.M22 + rhs.M22, lhs.M23 + rhs.M23,
+                               lhs.M30 + rhs.M30, lhs.M31 + rhs.M31, lhs.M32 + rhs.M32, lhs.M33 + rhs.M33);
+        }
+
+        /// <summary>
+        /// Subtracts the Matrix
+        /// </summary>
+        /// <param name="lhs">left hand Matrix</param>
+        /// <param name="rhs">Right hand Matrix</param>
+        /// <returns>returns the subtracted Matriexes</returns>
+        public static Matrix4 operator -(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4(lhs.M00 - rhs.M00, lhs.M01 - rhs.M01, lhs.M02 - rhs.M02, lhs.M03 - rhs.M03,
+                               lhs.M10 - rhs.M10, lhs.M11 - rhs.M11, lhs.M12 - rhs.M12, lhs.M13 - rhs.M13,
+                               lhs.M20 - rhs.M20, lhs.M21 - rhs.M21, lhs.M22 - rhs.M22, lhs.M23 - rhs.M23,
+                               lhs.M30 - rhs.M30, lhs.M31 - rhs.M31, lhs.M32 - rhs.M32, lhs.M33 - rhs.M33);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public static Vector4 operator *(Matrix4 lhs, Vector4 rhs)
+        {
+            return new Vector4();
+        }
+
+        /// <summary>
+        /// Multiplies the Matrixes but they order will allwayes be the left hand then the right
+        /// </summary>
+        /// <param name="lhs">Left hand is beeing scaled </param>
+        /// <param name="rhs">the right hand is the scaler </param>
+        /// <returns>The multipied Matrixes</returns>
+        public static Matrix4 operator *(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4
+                (
+                    ///The temps multiypy values is rows by coloms
+                    ///Row1, Colum1
+                    (lhs.M00 * rhs.M00) + (lhs.M01 * rhs.M10) + (lhs.M02 * rhs.M20) + (lhs.M03 * rhs.M30),
+                    //Row1 , column2
+                    (lhs.M00 * rhs.M01) + (lhs.M01 * rhs.M11) + (lhs.M02 * rhs.M21) + (lhs.M03 * rhs.M31),
+                    //Row1 , column 3
+                    (lhs.M00 * rhs.M02) + (lhs.M01 * rhs.M12) + (lhs.M02 * rhs.M22) + (lhs.M03 * rhs.M32),
+                    //Row1 , column 4
+                    (lhs.M00 * rhs.M03) + (lhs.M01 * rhs.M13) + (lhs.M02 * rhs.M23) + (lhs.M03 * rhs.M33),
+
+                    //Row2, columns1
+                    (lhs.M10 * rhs.M00) + (lhs.M11 * rhs.M10) + (lhs.M12 * rhs.M20) + (lhs.M13 * rhs.M30),
+                    //Row2, columns2
+                    (lhs.M10 * rhs.M01) + (lhs.M11 * rhs.M11) + (lhs.M12 * rhs.M21) + (lhs.M13 * rhs.M31),
+                    //Row2, columns3
+                    (lhs.M10 * rhs.M02) + (lhs.M11 * rhs.M12) + (lhs.M12 * rhs.M22) + (lhs.M13 * rhs.M32),
+                    //Row2, columns4
+                    (lhs.M10 * rhs.M03) + (lhs.M11 * rhs.M13) + (lhs.M12 * rhs.M23) + (lhs.M13 * rhs.M33),
+
+                    //Row3, colum1
+                    (lhs.M20 * rhs.M00) + (lhs.M21 * rhs.M10) + (lhs.M22 * rhs.M20) + (lhs.M23 * rhs.M30),
+                    //Row3, colum2
+                    (lhs.M20 * rhs.M01) + (lhs.M21 * rhs.M11) + (lhs.M22 * rhs.M21) + (lhs.M23 * rhs.M31),
+                    //Row3, colum3
+                    (lhs.M20 * rhs.M02) + (lhs.M21 * rhs.M12) + (lhs.M22 * rhs.M22) + (lhs.M23 * rhs.M32),
+                    //Row3, colum4
+                    (lhs.M20 * rhs.M03) + (lhs.M21 * rhs.M13) + (lhs.M22 * rhs.M23) + (lhs.M23 * rhs.M33),
+
+                    //Row3, colum1
+                    (lhs.M30 * rhs.M00) + (lhs.M31 * rhs.M10) + (lhs.M32 * rhs.M20) + (lhs.M33 * rhs.M30),
+                    //Row3, colum2
+                    (lhs.M30 * rhs.M01) + (lhs.M31 * rhs.M11) + (lhs.M32 * rhs.M21) + (lhs.M33 * rhs.M31),
+                    //Row3, colum3
+                    (lhs.M30 * rhs.M02) + (lhs.M31 * rhs.M12) + (lhs.M32 * rhs.M22) + (lhs.M33 * rhs.M32),
+                    //Row3, colum4
+                    (lhs.M30 * rhs.M03) + (lhs.M31 * rhs.M13) + (lhs.M32 * rhs.M23) + (lhs.M33 * rhs.M33)
+
+
+                );
+        }
+    }
+}
---
+++ MathLibaray/Vector4.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibaray
+{
+    public struct Vector4
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+
+        public Vector4(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+
+        /// <summary>
+        /// Gets the length of the vector
+        /// </summary>
+        public float Magnitude
+        {
+            get { return (float)Math.Sqrt(X * X + Y * Y + Z * Z + W * W); }
+        }
+
+        /// <summary>
+        /// Property that returns the normalized value of the vector2
+        /// </summary>
+        public Vector4 Normalized
+        {
+            get
+            {
+                Vector4 value = this;
+                return value.Normalize();
+            }
+        }
+
+        /// <summary>
+        /// Changes this vector to have a magnitude that is equal to one
+        /// </summary>
+        /// <returns>The result of the normalization
+        /// Returns an empty vector if the magnitude is zero</returns>
+        public Vector4 Normalize()
+        {
+            if (Magnitude == 0)
+                return new Vector4();
+
+             return this /= Magnitude;
+        }
+
+        /// <param name="lhs">The left hand side of the operation</param>
+        /// <param name="rhs">THe right hand side of the operation</param>
+        /// <returns>The dot product of the first vector onto the second</returns>
+        public static float DotProduct(Vector4 lhs, Vector4 rhs)
+        {
+            return (lhs.X * rhs.X) + (lhs.Y * rhs.Y) + (lhs.Z * rhs.Z);
+        }
+
+        public static Vector4 CrossProduct(Vector4 lhs, Vector4 rhs)
+        {
+            return new Vector4(lhs.Y * rhs.Z - lhs.Z * rhs.Y,
+                               lhs.Z * rhs.X - lhs.X * rhs.Z,
+                               lhs.X * rhs.Y - lhs.Y * rhs.X,
+                               0);
+        }
+
+        /// <param name="lhs">Left hand side of operation</param>
+        /// <param name="rhs">Right hand side of operation</param>
+        /// <returns>Returns the distance between two vectors</returns>
+        public static float Distance(Vector4 lhs, Vector4 rhs)
+        {
+            return (rhs - lhs).Magnitude;
+        }
+        /// <summary>
+        /// Adds the x value and they values of the second vector to the first
+        /// </summary>
+        /// <param name="lhs">Left hand vector4</param>
+        /// <param name="rhs">right hand vector4 that will be added to the left</param>
+        /// <returns>a new vector4 with the added X and Y variables</returns>
+        public static Vector4 operator +(Vector4 lhs, Vector4 rhs)
+        {
+            return new Vector4 { X = lhs.X + rhs.X, Y = lhs.Y + rhs.Y, Z = lhs.Z + rhs.Z , W = lhs.W + rhs.W};
+        }
+
+        /// <summary>
+        /// Subtracts the x value and they values of the second vector from the first
+        /// </summary>
+        /// <param name="lhs">Left hand vector4</param>
+        /// <param name="rhs">right hand vector4 that will be subtracted from the first</param>
+        /// <returns>a new vector4 with the subtracted variables</returns>
+        public static Vector4 operator -(Vector4 lhs, Vector4 rhs)
+        {
+            return new Vector4 { X = lhs.X - rhs.X, Y = lhs.Y - rhs.Y, Z = lhs.Z - rhs.Z, W = lhs.W - rhs.W };
+        }
+
+        /// <summary>
+        /// Multiplies the vector's X and Y values by the scalar
+        /// </summary>
+        /// <param name="vec3">The vector that is being scaled</param>
+        /// <param name="scalar">The value that the vector will be scaled by</param>
+        /// <returns>A new scaled vector</returns>
+        public static Vector4 operator *(Vector4 vec4, float scalar)
+        {
+            return new Vector4 { X = vec4.X * scalar, Y = vec4.Y * scalar, Z = vec4.Z * scalar, W = vec4.X * scalar };
+        }
+
+        /// <summary>
+        /// Divides the vector's X and Y values by the scalar
+        /// </summary>
+        /// <param name="vec4">The vector that is being scaled</param>
+        /// <param name="scalar">The value that the vector will be scaled by</param>
+        /// <returns>A new scaled vector</returns>
+        public static Vector4 operator /(Vector4 vec4, float scalar)
+        {
+            return new Vector4 { X = vec4.X / scalar, Y = vec4.Y / scalar, Z = vec4.Z, W = vec4.W / scalar};
+        }
+
+        /// <summary>
+        /// Checks to see if two vectors are equal to each other
+        /// </summary>
+        /// <param name="lhs">The vector on the left hand side</param>
+        /// <param name="rhs">The vector on the right hand side</param>
+        /// <returns>True if the vectors are equal to each other</returns>
+        public static bool operator ==(Vector4 lhs, Vector4 rhs)
+        {
+            return lhs.X == rhs.X && lhs.Y == rhs.Y && lhs.Z == rhs.Z && lhs.W == rhs.W;
+        }
+
+        /// <summary>
+        /// Checks to see if two vectors are not equal to each other
+        /// </summary>
+        /// <param name="lhs">The vector on the left hand side</param>
+        /// <param name="rhs">The vector on the right hand side</param>
+        /// <returns>True if the vectors are not equal to each other</returns>
+        public static bool operator !=(Vector4 lhs, Vector4 rhs)
+        {
+            return lhs.X != rhs.X || lhs.Y != rhs.Y || lhs.Z != rhs.Z || lhs.W != rhs.W;
+        }
+    }
+}
